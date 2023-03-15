5bc0eafd7a3b995f0397e48e446215e8d3c320b3
mytuan10 <102213470+mytuan10@users.noreply.github.com>
2023-03-15 14:14:09 +0700

    Add files via upload

---
+++ struct_qly_ds_SV1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct SinhVien
+{
+    char name[30];
+    int age;
+    char gt[5];
+    float dL, dT, dH;
+    float dtb = 0;
+}
+
+typedef SinhVien SV;
+
+void nhap(SV &sv);
+void xuat(SV a[], int n);
+void dtb(SV &sv);
+void xepLoai(SV a[], int n);
+void xuatFile(SV a[], int n, char FileName[]);
+
+int main()
+{
+    int key;
+    char fileName[] = "new_file.txt";
+    int n;
+    bool daNhap = false;
+    do
+    {
+        printf("Nhap so luong SV \n");
+        scanf("%d", &n);
+
+    } while {
+        
+
+
+        
+    };
+    
+}
+
+void nhap(int n)
+{
+    
+
+}
+
+
